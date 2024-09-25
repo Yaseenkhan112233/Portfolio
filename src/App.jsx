ee42ffcf86559cd63882eb303ad1999411683832
Yaseenkhan112233 <yk5793790@gmail.com>
2024-09-25 16:13:40 +0500

    4:13

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Header from "./Components/header";
+import Header from "./Components/Header";
 import "./app.css";
 import Hero from "./Components/Hero";
 import "bootstrap/dist/css/bootstrap.css";
